e4bdf1d774883ccf64fe9cb5cdab8f26310bec8b
ddai6315 <132826195+ddai6315@users.noreply.github.com>
2023-05-09 22:58:43 +0900

    Add files via upload

--- voicevox_app.py
+++ voicevox_app.py
@@ -1,6 +1,10 @@
 import streamlit as st
 import openai
-from voicevox import Voicevox
+import requests, json
+import io
+import wave
+import pyaudio
+import time
 
 # Streamlit Community Cloudの「Secrets」からOpenAI API keyを取得
 openai.api_key = st.secrets.OpenAIAPI.openai_api_key
@@ -27,6 +31,57 @@ def communicate():
     messages.append(bot_message)
 
     st.session_state["user_input"] = ""  # 入力欄を消去
+
+# 以下、Vicevoxを使用するための記述
+class Voicevox:
+    def __init__(self,host="127.0.0.1",port=50021):
+        self.host = host
+        self.port = port
+
+    def speak(self,text=None,speaker=47): # VOICEVOX:ナースロボ＿タイプＴ
+
+        params = (
+            ("text", text),
+            ("speaker", speaker)  # 音声の種類をInt型で指定
+        )
+
+        init_q = requests.post(
+            f"http://{self.host}:{self.port}/audio_query",
+            params=params
+        )
+
+        res = requests.post(
+            f"http://{self.host}:{self.port}/synthesis",
+            headers={"Content-Type": "application/json"},
+            params=params,
+            data=json.dumps(init_q.json())
+        )
+
+        # メモリ上で展開
+        audio = io.BytesIO(res.content)
+
+        with wave.open(audio,'rb') as f:
+            # 以下再生用処理
+            p = pyaudio.PyAudio()
+
+            def _callback(in_data, frame_count, time_info, status):
+                data = f.readframes(frame_count)
+                return (data, pyaudio.paContinue)
+
+            stream = p.open(format=p.get_format_from_width(width=f.getsampwidth()),
+                            channels=f.getnchannels(),
+                            rate=f.getframerate(),
+                            output=True,
+                            stream_callback=_callback)
+
+            # Voice再生
+            stream.start_stream()
+            while stream.is_active():
+                time.sleep(0.1)
+
+            stream.stop_stream()
+            stream.close()
+            p.terminate()
 
 # voicevox関数
 def sepak_voicevox(text):
